9292757f7e514ac22374f6c5ab4be9da2026c465
Javier Parada <javierparada73@outlook.com>
2020-04-06 16:52:26 +0200

    Add remaining correct tests for sum

--- test/unit/math/sum.spec.js
+++ test/unit/math/sum.spec.js
@@ -20,6 +20,12 @@ describe("unit.resolve.sum", () => {
       const result = resolve(operation)
       expect(result).to.be.equals(7)
     })
+
+    it("Should resolve sum with a negative element", () => {
+      const operation = ['+', -7]
+      const result = resolve(operation)
+      expect(result).to.be.equals(-7)
+    })
   })
 
   describe("Should resolve simple operations with two elements", () => {
@@ -57,6 +63,12 @@ describe("unit.resolve.sum", () => {
       const operation = ['+', 7, 9]
       const result = resolve(operation)
       expect(result).to.be.equals(16)
+    })
+
+    it("Should resolve sum with a negative element and another any element", () => {
+      const operation = ['+', -7, 9]
+      const result = resolve(operation)
+      expect(result).to.be.equals(2)
     })
   })
 
@@ -107,6 +119,24 @@ describe("unit.resolve.sum", () => {
       const operation = ['+', 2, 2, 2]
       const result = resolve(operation)
       expect(result).to.be.equals(6)
+    })
+
+    it("Should resolve sum with some elements value but one of them negative", () => {
+      const operation = ['+', 2, -2, 2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(2)
+    })
+
+    it("Should resolve sum with a positive element", () => {
+      const operation = ['+', 2, -2, -2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(-2)
+    })
+
+    it("Should resolve sum with negative elements with same value but abs greater than 1", () => {
+      const operation = ['+', -2, -2, -2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(-6)
     })
   })
 
@@ -160,8 +190,62 @@ describe("unit.resolve.sum", () => {
     })
   })
 
-  describe("Should throw error", () => {
+  describe("Should resolve a single nested operations", () => {
+    it("Should resolve nested operations without any other element", () => {
+      const operation = ['+', ['+', 7, 7]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(14)
+    })
 
+    it("Should resolve nested operation at head with more elements", () => {
+      const operation = ['+', ['+', 5, 5], 1, 2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(13)
+    })
+
+    it("Should resolve nested operation at middle with more elements", () => {
+      const operation = ['+', 1, ['+', 5, 5], 2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(13)
+    })
+
+    it("Should resolve nested operation at tail with more elements", () => {
+      const operation = ['+', 1, 2, ['+', 5, 5]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(13)
+    })
+  })
+
+  describe("Should resolve double nested operations", () => {
+    it("Should resolve nested operations without any other element", () => {
+      const operation = ['+', ['+', 7, 7], ['+', 5, 5]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(24)
+    })
+
+    it("Should resolve consecutive nested operation from head with more elements", () => {
+      const operation = ['+', ['+', 5, 5], ['+', 7, 7], 2]
+      const result = resolve(operation)
+      expect(result).to.be.equals(26)
+    })
+
+    it("Should resolve consecutive nested operation from tail with more elements", () => {
+      const operation = ['+', 2, ['+', 5, 5], ['+', 7, 7]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(26)
+    })
+
+    it("Should resolve nested operations with element between them", () => {
+      const operation = ['+', ['+', 5, 5], 2, ['+', 7, 7]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(26)
+    })
+
+    it("Should resolve double nested operations", () => {
+      const operation = ['+', 1, ['+', 2, ['+', 3, 4]]]
+      const result = resolve(operation)
+      expect(result).to.be.equals(10)
+    })
   })
 })
 
