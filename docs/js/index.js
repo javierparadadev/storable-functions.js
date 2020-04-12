ccf7baef2bac12738ffe78923b825437e1cab289
Javier Parada <javierparada73@outlook.com>
2020-04-12 19:39:34 +0200

    Add playground to documentation and fix library name

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,11 +2,20 @@ var app = new Vue({
   el: '#app',
   data: {
     test: 'Hello test!',
-    tab: 'documentation'
+    tab: 'playground',
+    code: '',
+    solution: ''
   },
   methods: {
     setTab: function(currentTab) {
       this.tab = currentTab
+    },
+    resolve: function() {
+      try {
+        this.solution = sf.resolve(JSON.parse(this.code.replace(/'/g, '"')))
+      } catch (err) {
+        this.solution = ''
+      }
     }
   }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     filename: 'storable-functions.js',
     path: path.resolve(__dirname, './docs/js/'),
+    library: 'sf'
   },
   mode: 'production'
 }
