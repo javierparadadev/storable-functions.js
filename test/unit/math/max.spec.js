fcb404a28eee2ad2ec47d6954702aa8db6edb613
mpardalm <mpardalm@outlook.com>
2020-05-07 19:43:31 +0200

    Update functions

--- test/unit/math/max.spec.js
+++ test/unit/math/max.spec.js
@@ -4,43 +4,43 @@ const { resolve } = require('../../../index')
 describe("unit.resolve.max", () => {
   describe("Should resolve simple operations", () => {
     it("Should resolve max with 1 element", () => {
-      const operation = ['max', 0]
+      const operation = ['max', ['array', 0]]
       const result = resolve(operation)
       expect(result).to.be.equals(0)
     })
 
     it("Should resolve max with first element greater than second", () => {
-      const operation = ['max', 1, 0]
+      const operation = ['max', ['array', 1, 0]]
       const result = resolve(operation)
       expect(result).to.be.equals(1)
     })
 
     it("Should resolve max with second element greater than second", () => {
-      const operation = ['max', 1, 2]
+      const operation = ['max', ['array', 1, 2]]
       const result = resolve(operation)
       expect(result).to.be.equals(2)
     })
 
     it("Should resolve max at least one negative number", () => {
-      const operation = ['max', 1, -1]
+      const operation = ['max', ['array', 1, -1]]
       const result = resolve(operation)
       expect(result).to.be.equals(1)
     })
 
     it("Should resolve max with more than one negative number", () => {
-      const operation = ['max', -1, -2]
+      const operation = ['max', ['array', -1, -2]]
       const result = resolve(operation)
       expect(result).to.be.equals(-1)
     })
 
     it("Should resolve max all numbers with same value ", () => {
-      const operation = ['max', 1, 1]
+      const operation = ['max', ['array', 1, 1]]
       const result = resolve(operation)
       expect(result).to.be.equals(1)
     })
 
     it("Should resolve max more than 2 numbers", () => {
-      const operation = ['max', 1, -3, 2, -8]
+      const operation = ['max', ['array', 1, -8, 2, -3]]
       const result = resolve(operation)
       expect(result).to.be.equals(2)
     })
